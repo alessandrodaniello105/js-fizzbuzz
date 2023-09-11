3e40b8bff835c89cc80844a533ba8f4bc2c00d01
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-11 15:22:21 +0200

    [add]: conditions style; [edit]: add not to if

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 const boxesContainer = document.querySelector('.boxes');
 let boxEl;
 
-console.log(boxEl)
-
 for (let i = 1; i <= 30; i++) {
-  console.log(i);
+
   boxEl = document.createElement('div');
   boxEl.classList.add('box');
   boxEl.append(i);
 
-  if (i % 15 === 0) {
+  if (!(i % 15)) {
 
     boxEl.innerHTML = '';
     boxEl.append('FizzBuzz');
+    boxEl.classList.add('fizzbuzz');
     
-  } else if (i % 5 === 0) {
+  } else if (!(i % 5)) {
 
     boxEl.innerHTML = '';
     boxEl.append('Buzz');
+    boxEl.classList.add('buzz');
 
-  } else if ((i % 3 === 0)) {
+  } else if (!(i % 3)) {
 
     boxEl.innerHTML = '';
     boxEl.append('Fizz');
+    boxEl.classList.add('fizz');
 
   }
 
   boxesContainer.append(boxEl);
 }
-// boxesContainer.append('boxEl');
