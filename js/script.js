14c5b56bc3177a17322301c29a999c8d2a4641fc
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-11 15:12:58 +0200

    [add]: Fizz/Buzz/FizzBuzz condition

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('Hello World!')
+const boxesContainer = document.querySelector('.boxes');
+let boxEl;
+
+console.log(boxEl)
+
+for (let i = 1; i <= 30; i++) {
+  console.log(i);
+  boxEl = document.createElement('div');
+  boxEl.classList.add('box');
+  boxEl.append(i);
+
+  if (i % 15 === 0) {
+
+    boxEl.innerHTML = '';
+    boxEl.append('FizzBuzz');
+    
+  } else if (i % 5 === 0) {
+
+    boxEl.innerHTML = '';
+    boxEl.append('Buzz');
+
+  } else if ((i % 3 === 0)) {
+
+    boxEl.innerHTML = '';
+    boxEl.append('Fizz');
+
+  }
+
+  boxesContainer.append(boxEl);
+}
+// boxesContainer.append('boxEl');
